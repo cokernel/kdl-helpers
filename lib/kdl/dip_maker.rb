24cd89ff1dad5e357899befd210c4fe653ac730b
Michael Slone <m.slone@gmail.com>
2014-08-19 13:08:26 -0400

    fix typo

--- lib/kdl/dip_maker.rb
+++ lib/kdl/dip_maker.rb
@@ -194,13 +194,13 @@ module KDL
             ref_href = "#{base}.mp3"
             @mets.add_file :fileGrp => fileGrp_id,
                            :use => 'reference audio',
-                           :file => #thumb_href,
+                           :file => ref_href,
                            :mimetype => 'audio/mpeg'
 
             sref_href = "#{base}.ogg"
             @mets.add_file :fileGrp => fileGrp_id,
                            :use => 'secondary reference audio',
-                           :file => #front_thumb_href,
+                           :file => sref_href,
                            :mimetype => 'audio/ogg'
 
             master_id = @mets.file_id :fileGrp => fileGrp_id,
