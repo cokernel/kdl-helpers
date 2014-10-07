690c060ba06a47746eac7245267f6dda2aa43da0
Michael Slone <m.slone@gmail.com>
2014-10-07 13:39:53 -0400

    revert

--- lib/kdl/normalizer.rb
+++ lib/kdl/normalizer.rb
@@ -1,6 +1,6 @@
 module KDL
   module Normalizer
-    def self.normalize thing
+    def normalize thing
       # I know it is not idiomatic Ruby
       # to care about specific classes.
       #
@@ -22,10 +22,6 @@ module KDL
       else
         thing
       end
-    end
-
-    def normalize thing
-      self.normalize thing
     end
   end
 end
